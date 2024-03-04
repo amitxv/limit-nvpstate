675371685a35ff19e6cebc7ffc4e11dbf6163448
AMIT <86924251+amitxv@users.noreply.github.com>
2024-03-04 23:19:20 +0000

    create build.ps1 script

---
+++ .github/workflows/build.ps1
@@ -0,0 +1,14 @@
+function main() {
+    # build application
+    MSBuild.exe .\limit-nvpstate\limit-nvpstate.sln -p:Configuration=Release -p:Platform=x64
+
+    mkdir build\limit-nvpstate
+    Move-Item .\limit-nvpstate\limit-nvpstate\bin\x64\Release\limit-nvpstate.exe build\limit-nvpstate
+    Move-Item .\limit-nvpstate\limit-nvpstate\bin\x64\Release\nvidiaInspector.exe build\limit-nvpstate
+
+    return 0
+}
+
+$_exitCode = main
+Write-Host # new line
+exit $_exitCode
