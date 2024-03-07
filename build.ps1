15f810242bf121960d8e25bef95ad6176eb6ec4c
AMIT <86924251+amitxv@users.noreply.github.com>
2024-03-07 23:08:54 +0000

    update build script

--- build.ps1
+++ build.ps1
@@ -3,7 +3,7 @@ function main() {
     MSBuild.exe ".\limit-nvpstate\limit-nvpstate.sln" -p:Configuration=Release -p:Platform=x64
 
     if (Test-Path ".\build\") {
-        Remove-Item -Path ".\build\" -Recurse
+        Remove-Item -Path ".\build\" -Recurse -Force
     }
 
     # create folder structure
