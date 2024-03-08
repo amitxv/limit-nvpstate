5ad796b23cdc3889d180e6fc05549c67ff52a7af
AMIT <86924251+amitxv@users.noreply.github.com>
2024-03-08 11:15:19 +0000

    Update build script

--- build.ps1
+++ build.ps1
@@ -9,15 +9,15 @@ function main() {
         return 1
     }
 
-    # build application
-    MSBuild.exe ".\limit-nvpstate\limit-nvpstate.sln" -p:Configuration=Release -p:Platform=x64
-
     if (Test-Path ".\build\") {
         Remove-Item -Path ".\build\" -Recurse -Force
     }
 
     # create folder structure
-    New-Item -ItemType Directory -Path ".\build\limit-nvpstate\"
+    mkdir ".\build\limit-nvpstate\"
+
+    # build application
+    MSBuild.exe ".\limit-nvpstate\limit-nvpstate.sln" -p:Configuration=Release -p:Platform=x64
 
     # create final package
     Copy-Item ".\limit-nvpstate\limit-nvpstate\bin\x64\Release\limit-nvpstate.exe" ".\build\limit-nvpstate\"
